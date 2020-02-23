b6f5cfa5ff0ef8db3c23cce1e2ae43addbb97aa0
skypyb <pyb068@gmail.com>
2020-02-23 21:10:03 +0800

    [roacg-service/gateway] Add security dependency

---
+++ roacg-service/service-gateway/src/main/java/com/roacg/service/gateway/security/WebFluxSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.roacg.service.gateway.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+/**
+ * https://docs.spring.io/spring-security/site/docs/current/reference/html5
+ */
+
+@Configuration
+@EnableWebFluxSecurity
+public class WebFluxSecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+
+        return http.build();
+    }
+}

4fce9adfb034c0c06a4d1cbdbfd77ff24fccd45e
skypyb <pyb068@gmail.com>
2020-02-24 20:18:21 +0800

    [roacg-service] Add auth service; dependency update

---
+++ roacg-service/service-auth/src/main/java/com/roacg/service/auth/AuthSystemApplication.java
@@ -0,0 +1,11 @@
+package com.roacg.service.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthSystemApplication.class, args);
+    }
+}
